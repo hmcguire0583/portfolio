bce33e1a1f16fab5eaa6a4c22a2f4efa2e9f706a
hmcguire0583 <harry_mcguire@student.uml.edu>
2025-06-25 13:26:26 -0400

    ok

--- react-website-portfolio-1/src/components/Navbar.js
+++ react-website-portfolio-1/src/components/Navbar.js
@@ -9,37 +9,12 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            Harry McGuire <i className="fab fa-typo3" />
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/projects" className="nav-links" onClick={closeMobileMenu}>
-                Projects
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
-                Contact
-              </Link>
-            </li>
-          </ul>
+    <>      
+    <nav className = "navbar">
+        <div className = "navbar-container">
+        <Link to="/" className="navbar-logo">
+        My Projects <i className = 'fab fa-typo3' />  
+        </Link>
         </div>
       </nav>
     </>
