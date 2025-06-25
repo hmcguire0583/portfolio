9d3e67a71e385071d22e912d40642f96a70a9f45
Harry Jr. <130195397+hmcguire0583@users.noreply.github.com>
2025-06-25 14:43:38 -0400

    Remove Logo from Navbar

--- react-website-portfolio-1/src/components/Navbar.js
+++ react-website-portfolio-1/src/components/Navbar.js
@@ -12,9 +12,7 @@ function Navbar() {
     <>      
     <nav className = "navbar">
         <div className = "navbar-container">
-        <Link to="/" className="navbar-logo">
-        My Projects <i className = 'fab fa-typo3' />  
-        </Link>
+        {/* Logo removed */}
         </div>
       </nav>
     </>
