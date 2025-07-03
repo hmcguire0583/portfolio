6ff2d02fdcac17be52a08933897aee021351bc17
hmcguire0583 <harry_mcguire@student.uml.edu>
2025-07-03 16:55:04 -0400

    yo:

--- react-website-portfolio-1/src/App.js
+++ react-website-portfolio-1/src/App.js
@@ -16,8 +16,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" exact/>
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home />} exact />
           <Route path="/me" element={<Me />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
