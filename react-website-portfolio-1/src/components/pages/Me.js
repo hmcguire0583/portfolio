d25f25483416f84b5992107f1c625bb5b44b47b0
hmcguire0583 <harry_mcguire@student.uml.edu>
2025-07-08 22:13:11 -0400

    ok

--- react-website-portfolio-1/src/components/pages/Me.js
+++ react-website-portfolio-1/src/components/pages/Me.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import myImage from '../../images/IMG_2825.jpeg'; 
 
 export default function Me() {
-    return <h1 className='me'>About Me</h1>;
+    return (
+    <>  
+    <h1 class='me'>About Me</h1>
+    <img src={myImage} alt="My Image" width="200" height="250" />
+    <p>This is a simple HTML page.</p>
+    <img src="https://moderndiplomacy.eu/wp-content/uploads/2019/01/rolex-oyster.jpg" alt="Rolex Watch" width="200" height="200" />
+    </>
+    
+    );
+
 }
