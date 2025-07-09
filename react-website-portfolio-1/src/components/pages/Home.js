bad0eb4e55098fc43882da1bf2114f60ac712113
hmcguire0583 <harry_mcguire@student.uml.edu>
2025-07-09 11:02:44 -0400

    ok

--- react-website-portfolio-1/src/components/pages/Home.js
+++ react-website-portfolio-1/src/components/pages/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../../App.css';
+import Typed from 'typed.js';
 
 /**
  * This component serves as the landing page for the portfolio website.
  */
 export default function Home() {
-return (
+  const typedEl = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(typedEl.current, {
+      strings: ['Welcome to my website!', 'I am a fullstack-developer', 'I have a passion for OOP', ' '],
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: true
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
     <>
-        <h1 className="home">Home</h1>
-        <div className="bg-black">
-        <h2 className="text-6xl font-extrabold text-white drop-shadow-lg">NOW</h2>
-        </div>
+    <h1 className="text-4xl font-bold text-center mt-32 text-blue-300">
+    <span ref={typedEl}></span>
+    </h1>
     </>
-);
+  );
 }
