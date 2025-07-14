d9042c788ca860c13588f23f1bbe8baf1b60d969
hmcguire0583 <harry_mcguire@student.uml.edu>
2025-07-14 14:24:12 -0400

    push changes

--- react-website-portfolio-1/src/components/pages/Me.js
+++ react-website-portfolio-1/src/components/pages/Me.js
@@ -5,18 +5,13 @@ import myImage from '../../images/IMG_2825.jpeg';
 export default function Me() {
     return (
     <>  
-    <div class="bg-transparent border shadow-teal-300 shadow-lg max-2xl p-6 rounded-lg dark:bg-gray-700 dark:text-gray-300">
-    <h1 class="text-4xl text-blue-300 font-semibold
-     font-extrabold py-3">Tailwind Playground</h1>
-  </div>
     <h1 class='me'>About Me</h1>
     <img src={myImage} alt="My Image" width="200" height="250" />
     <p className='text-center text-sky-200 text-bold 
     '>This is a simple HTML page.</p>
     <img src="https://moderndiplomacy.eu/wp-content/uploads/2019/01/rolex-oyster.jpg" alt="Rolex Watch" width="200" height="200" />
-    <div class=" dark:bg-gray-800 flex justify-center items-center w-screen h-screen p-5">
-  
-</div>
+    <div className="dark:bg-gray-800 flex justify-center items-center w-screen h-screen p-5">
+    </div>
     </>
     
     );
