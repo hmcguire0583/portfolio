8cba03bd4c63be5421420c46c4c29915fa8404e2
hmcguire0583 <harryjunior2026@gmail.com>
2025-08-05 00:51:56 -0400

    prevent gsap scrolling

--- react-website-portfolio-1/src/components/pages/Home.js
+++ react-website-portfolio-1/src/components/pages/Home.js
@@ -121,7 +121,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen px-4">
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen px-4 overflow-hidden">
       <div className="flex-1 flex flex-col items-center md:items-start mb-10 md:mb-0">
         <h1 className="text-4xl md:text-5xl font-bold mb-6 from-teal-300 to-blue-500 bg-gradient-to-r
           bg-clip-text text-transparent pb-1 dark:from-red-500 dark:to-red-700">
